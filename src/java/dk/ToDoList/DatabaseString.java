6dbcf40002f0ee642e4b4f897df70825eebb6c09
enshuti <enochnshuti@gmail.com>
2015-09-07 09:45:35 +0200

    data

--- src/java/dk/ToDoList/DatabaseString.java
+++ src/java/dk/ToDoList/DatabaseString.java
@@ -4,5 +4,5 @@ public class DatabaseString {
     public final static String Protocol = "mysql"; // The protocol
     public final static String serverURL = "//localhost/ToDoList"; // The server localtion 
     public final static String username = "root"; // Server's user
-    public final static String password = "rootPassword"; // User's password
+    public final static String password = "admin"; // User's password
 }
