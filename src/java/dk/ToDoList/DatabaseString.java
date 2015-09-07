6a49cd63a74a6ddc65b6ac3846176f42e3cd80e7
Patrick <XDRosenheim@users.noreply.github.com>
2015-09-07 08:09:03 +0200

    Skal ikke updates.

--- src/java/dk/ToDoList/DatabaseString.java
+++
@@ -1,8 +0,0 @@
-package dk.ToDoList;
-public class DatabaseString {
-    public final static String Driver = "jdbc"; // The driver
-    public final static String Protocol = "mysql"; // The protocol
-    public final static String serverURL = "//localhost/ToDoList"; // The server localtion 
-    public final static String username = "######USERNAME######"; // Server's user
-    public final static String password = "######PASSWORD######"; // User's password
-}
