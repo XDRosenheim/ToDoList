3477b7253274b9c642616727e96f8d8a00a18fff
Patrick <XDRosenheim@users.noreply.github.com>
2015-09-07 08:40:51 +0200

    FLuck.

---
+++ src/java/dk/ToDoList/DatabaseString.java
@@ -0,0 +1,8 @@
+package dk.ToDoList;
+public class DatabaseString {
+    public final static String Driver = "jdbc"; // The driver
+    public final static String Protocol = "mysql"; // The protocol
+    public final static String serverURL = "//localhost/ToDoList"; // The server localtion 
+    public final static String username = "root"; // Server's user
+    public final static String password = "rootPassword"; // User's password
+}
